aa459cbfc6037dfb0c86517013adfbb0ac2c54da
崔伟杰 <cui.wj@aiiage.com>
2018-07-23 21:06:32 +0800

    未完成的UI

--- app/src/main/java/com/example/aiiage/drawerlayoutdemo/TestActivity.java
+++ app/src/main/java/com/example/aiiage/drawerlayoutdemo/TestActivity.java
@@ -8,6 +8,6 @@ public class TestActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.item);
+        setContentView(R.layout.item_loader);
     }
 }
